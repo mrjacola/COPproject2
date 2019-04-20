94426b70827d94261f6125efcfb0ec81f708132c
Jacob Olander <31401560+mrjacola@users.noreply.github.com>
2019-04-20 20:58:01 +0200

    Add files via upload

--- vmc_he_atom.py
+++ vmc_he_atom.py
@@ -2,24 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def energy_local(r1, r2, alpha):
+def energy_local(r, alpha):
 
-    r1_unit_vec = np.multiply(r1.T, 1/np.linalg.norm(r1, axis=1)).T
-    r2_unit_vec = np.multiply(r2.T, 1/np.linalg.norm(r2, axis=1)).T
-    r12 = r1 - r2
+    r1_unit_vec = np.multiply(r[:,:,0].T, 1/np.linalg.norm(r[:,:,0], axis=1)).T
+    r2_unit_vec = np.multiply(r[:,:,1].T, 1/np.linalg.norm(r[:,:,1], axis=1)).T
+    r12 = r[:,:,0] - r[:,:,1]
     r12_norm = np.linalg.norm(r12, axis=1)
     return -4 + np.einsum('ij,ij->i', r1_unit_vec - r2_unit_vec, r12)/(r12_norm * (1 + alpha * r12_norm)**2) - \
            1/(r12_norm * (1 + alpha * r12_norm)**3) - 1/(4 * (1 + alpha * r12_norm)**4) + 1/r12_norm
 
 
-def trans_prob(r1, r2, r1_p, r2_p, alpha):
+def trans_prob(r, r_p, alpha):
 
-    r1_norm = np.linalg.norm(r1, axis=1)
-    r2_norm = np.linalg.norm(r2, axis=1)
-    r1_p_norm = np.linalg.norm(r1_p, axis=1)
-    r2_p_norm = np.linalg.norm(r2_p, axis=1)
-    r12_norm = np.linalg.norm(r1 - r2, axis=1)
-    r12_p_norm = np.linalg.norm(r1_p - r2_p, axis=1)
+    r1_norm = np.linalg.norm(r[:,:,0], axis=1)
+    r2_norm = np.linalg.norm(r[:,:,1], axis=1)
+    r1_p_norm = np.linalg.norm(r_p[:,:,0], axis=1)
+    r2_p_norm = np.linalg.norm(r_p[:,:,1], axis=1)
+    r12_norm = np.linalg.norm(r[:,:,0] - r[:,:,1], axis=1)
+    r12_p_norm = np.linalg.norm(r_p[:,:,0] - r_p[:,:,1], axis=1)
 
     psi = np.exp(-2*(r1_norm + r2_norm) + r12_norm/(2*(1 + alpha * r12_norm)))
     psi_p = np.exp(-2*(r1_p_norm + r2_p_norm) + r12_p_norm/(2*(1 + alpha * r12_p_norm)))
@@ -30,12 +30,11 @@ def trans_prob(r1, r2, r1_p, r2_p, alpha):
 def vmc(num_walkers, num_mc_steps, num_therm_steps, alpha, new_walker_std=1.0):
 
     if num_mc_steps <= num_therm_steps:
-        print("ERROR: Number of steps must be greater than number of thermalizing steps")
+        print("ERROR: Number of MC steps must be greater than number of thermalizing steps")
         return 0, 0
 
     # Initialize walkers and energy
-    walkers1 = np.random.rand(num_walkers, 3)
-    walkers2 = np.random.rand(num_walkers, 3)
+    walkers = np.random.rand(num_walkers, 3, 2)
     energy_estimator = np.zeros([num_mc_steps-num_therm_steps, num_walkers])
     num_accept_jumps = 0
 
@@ -46,19 +45,16 @@ def vmc(num_walkers, num_mc_steps, num_therm_steps, alpha, new_walker_std=1.0):
             print("Progress: " + str(progress) + "%")
 
         # Randomly create new walkers
-        new_walkers1 = np.random.normal(walkers1, scale=new_walker_std)
-        new_walkers2 = np.random.normal(walkers2, scale=new_walker_std)
-
+        new_walkers = np.random.normal(walkers, scale=new_walker_std)
 
         # Test new walkers and update
-        update_conditions = np.random.rand(num_walkers) < trans_prob(walkers1, walkers2, new_walkers1, new_walkers2, alpha)
-        walkers1[update_conditions,:] = new_walkers1[update_conditions,:]
-        walkers2[update_conditions,:] = new_walkers2[update_conditions,:]
+        update_conditions = np.random.rand(num_walkers) < trans_prob(walkers, new_walkers, alpha)
+        walkers[update_conditions, :, :] = new_walkers[update_conditions, :, :]
         num_accept_jumps += np.sum(update_conditions)
 
         # Evaluate local energy at walkers positions
         if step_MC >= num_therm_steps:
-            energy_estimator[step_MC - num_therm_steps, :] = energy_local(walkers1, walkers2, alpha)
+            energy_estimator[step_MC - num_therm_steps, :] = energy_local(walkers, alpha)
 
     accept_ratio = num_accept_jumps / (num_walkers*num_mc_steps)
 
@@ -66,10 +62,10 @@ def vmc(num_walkers, num_mc_steps, num_therm_steps, alpha, new_walker_std=1.0):
 
 
 if __name__ == "__main__":
-    num_walk = 500
+    num_walk = 100
     num_steps = 20000
     therm_steps = 5000
-    alphas = np.linspace(0.05, 0.25, num=50)
+    alphas = np.linspace(0.05, 0.25, num=30)
     energies = np.zeros(len(alphas))
     energies_std = np.zeros(len(alphas))
     accept_rats = np.zeros(len(alphas))
